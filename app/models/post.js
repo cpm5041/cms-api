88a8ffcf782561d08b7c855184f438cf221bedd2
rrdaniels85 <rachel.daniels@libertymutual.com>
2017-05-16 10:11:06 -0400

    Creates route, model & controller for posts
    Creates model for posts and adds post to the router. Create post
    controller with create, index, show, update, and destroy actions.

---
+++ app/models/post.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true,
+  // do we even need this??
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+// NEED THIS??
+// postSchema.virtual('length').get(function length () {
+//   return this.text.length
+// })
+
+const Post = mongoose.model('Post', postSchema)
+
+module.exports = Post
